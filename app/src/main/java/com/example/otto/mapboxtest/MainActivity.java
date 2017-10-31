2de0ec259e1bedf96777e4d9a6953dbea24c6dbe
odexi <otakar93@gmail.com>
2017-10-31 12:18:48 +0200

    Enable/disable marker adding Popup window and remove markers

--- app/src/main/java/com/example/otto/mapboxtest/MainActivity.java
+++ app/src/main/java/com/example/otto/mapboxtest/MainActivity.java
@@ -1,26 +1,22 @@
 package com.example.otto.mapboxtest;
 
-import android.content.Context;
 import android.os.Parcel;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
-/*import com.mapbox.mapboxsdk.Mapbox;
-import com.mapbox.mapboxsdk.annotations.MarkerOptions;
-import com.mapbox.mapboxsdk.annotations.MarkerViewOptions;
-import com.mapbox.mapboxsdk.camera.CameraUpdate;
-import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
-import com.mapbox.mapboxsdk.constants.Style;
-import com.mapbox.mapboxsdk.maps.MapView;*/
-import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.IconFactory;
 import com.mapbox.mapboxsdk.annotations.Marker;
-import com.mapbox.mapboxsdk.annotations.MarkerViewOptions;
 import com.mapbox.mapboxsdk.annotations.BaseMarkerOptions;
-import com.mapbox.mapboxsdk.annotations.Marker;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
-import com.mapbox.mapboxsdk.annotations.MarkerViewOptions;
 import com.mapbox.mapboxsdk.camera.CameraPosition;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
@@ -31,53 +27,34 @@ import com.steerpath.sdk.maps.SteerpathAnnotationOptions;
 import com.steerpath.sdk.maps.SteerpathMap;
 import com.steerpath.sdk.maps.SteerpathMapFragment;
 import com.steerpath.sdk.maps.SteerpathMapView;
-import com.steerpath.sdk.maps.internal.SteerpathIcon;
-import com.steerpath.sdk.maps.internal.SteerpathMarkerViewOptions;
-import com.steerpath.sdk.meta.MetaFeature;
-import com.steerpath.sdk.telemetry.TelemetryConfig;
 
-import java.util.ArrayList;
-import java.util.List;
-/*import com.mapbox.mapboxsdk.geometry.LatLng;
-import com.mapbox.mapboxsdk.maps.MapView;
-import com.mapbox.mapboxsdk.maps.MapboxMap;
-import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;*/
-
+import static android.view.View.VISIBLE;
 
 
 public class MainActivity extends AppCompatActivity implements SteerpathMapFragment.MapViewListener{
 
-    private MapboxMap mbmap;
     private SteerpathMap map;
-    private SteerpathMapView mapView;
-    private List<SteerpathAnnotation> anns = new ArrayList<>();
-    BaseMarkerOptions baseMarkerOptions;
-
-    public static final String EXTRAS_HELPER_BUILDING = "com.steerpath.dev.helper.building";
-
+    private Marker marker;
+    private Marker markerToRemove;
+    Button removeMarker;
+    private Button disableMarkerAdd;
+    boolean addableMarkers = true;
+    boolean popupWindowActive = false;
     private String apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZXMiOiJiYXNlOnI7anVoYW5pdGVzdF9zdGF0aWM6cjtqdWhhbml0ZXN0X2R5bmFtaWM6ciIsImp0aSI6ImUyZjY1Mjc0LTY4YTgtNGM0ZS04MGY5LTUzNThkOTBiNTRkNSIsInN1YiI6Imp1aGFuaXRlc3QifQ.gAOJ1h7q43p65H3pXMYsZ2EGYCoCGUTcNhB5aX1s8j4";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       // mapboxMap.getInstance(this, "pk.eyJ1Ijoib2RleGkiLCJhIjoiY2o5NnJ4eG00MDdlMjJxcXNuOTJpYW5heiJ9.nsA86RhtKSpr503V5Y-zbQ");
 
-       setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
-
-       /* mapView = (SteerpathMapView) findViewById(R.id.mapView);
-        mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(this);
-        // mapView.setStyleUrl(Style.MAPBOX_STREETS);
-*/
+        disableMarkerAdd = (Button)findViewById(R.id.disableMarkerAdd);
 
         SteerpathClient.StartConfig config =  new SteerpathClient.StartConfig.Builder()
                 // MANDATORY:
                 .apiKey(apiKey)
                 .monitor(true)
-
                 .build();
-
 
         SteerpathClient.getInstance().start(this, config, new SteerpathClient.StartListener() {
             @Override
@@ -92,11 +69,10 @@ public class MainActivity extends AppCompatActivity implements SteerpathMapFragm
 
     @Override
     public void onMapViewReady(final SteerpathMapView mapView) {
-        this.mapView = mapView;
         mapView.getMapAsync(new OnMapReadyCallback() {
             @Override
-            public void onMapReady(final SteerpathMap map) {
-
+            public void onMapReady(final SteerpathMap smap) {
+                map = smap;
                 // enable positioning
                 map.setMyLocationEnabled(true);
 
@@ -108,30 +84,109 @@ public class MainActivity extends AppCompatActivity implements SteerpathMapFragm
 
                 map.setCameraPosition(position);
 
-                map.setOnMapClickListener(new MapboxMap.OnMapClickListener() {
+                mapView.setOnMapClickListener(new MapboxMap.OnMapClickListener() {
                     @Override
                     public void onMapClick(@NonNull LatLng point) {
-                        MetaFeature feature = mapView.getRenderedMetaFeature(point);
+                        final BaseMarkerOptions baseMarkerOptions = new BaseMarkerOptions() {
+                            @Override
+                            public BaseMarkerOptions getThis() {
+                                return this;
+                            }
 
-                        map.addAnnotation(new BaseMarkerOptions<Marker, >().position(point));
+                            @Override
+                            public Marker getMarker() {
+                                return marker;
+                            }
+
+                            @Override
+                            public int describeContents() {
+                                return 0;
+                            }
+
+                            @Override
+                            public void writeToParcel(Parcel dest, int flags) {
+
+                            }
+                        };
+
+                        if (addableMarkers) {
+                            marker = new Marker(baseMarkerOptions);
+                            marker.setPosition(point);
+                            marker.setIcon(IconFactory.getInstance(getApplicationContext()).fromResource(R.drawable.ic_accuracy_marker));
+
+                            SteerpathAnnotationOptions.Builder builder = new SteerpathAnnotationOptions.Builder();
+
+                            SteerpathAnnotationOptions steerpathAnnotationOptions = builder.userId("1").floor(0).withBaseMarkerOptions(baseMarkerOptions).build();
+
+                            map.addAnnotation(steerpathAnnotationOptions);
+                        }
 
                     }
                 });
+                mapView.setOnMarkerClickListener(new MapboxMap.OnMarkerClickListener() {
+                    @Override
+                    public boolean onMarkerClick(@NonNull Marker marker) {
+                        markerToRemove = marker;
+
+                        showPopupWindow(markerToRemove);
+
+                        return false;
+                    }
+                });
+
+
+
+
+
+                disableMarkerAdd.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (addableMarkers) {
+                            addableMarkers = false;
+                            disableMarkerAdd.setText("Enable marker add");
+                        } else {
+                            addableMarkers = true;
+                            disableMarkerAdd.setText("Disable marker add");
+                        }
+                    }
+                });
+
             }
         });
     }
 
-  /*  @Override
-    public void onMapReady(MapboxMap map) {
-        Log.d("MENNÄÄKS", "TÄNNE");
-        mapboxMap = map;
+    public void showPopupWindow(Marker marker) {
+        this.marker = marker;
+        LinearLayout mainLayout = (LinearLayout) findViewById(R.id.fragment_parent);
 
-        mapboxMap.setStyle(Style.SATELLITE);
-        LatLng koulu = new LatLng(60.221171,24.8057408);
-        mapboxMap.moveCamera(CameraUpdateFactory.newLatLngZoom(koulu,16));
-        mapboxMap.addMarker(new MarkerOptions()
-                .position(new LatLng(60.221171,24.8057408)));
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.popup_window, null);
 
-    }*/
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true;
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
 
+        popupWindow.showAtLocation(mainLayout, Gravity.CENTER, 0, 0);
+
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
+
+        final Button removeMarker = (Button)popupView.findViewById(R.id.deleteMarker);
+
+        removeMarker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                markerToRemove.remove();
+                popupWindow.dismiss();
+            }
+        });
+
+    }
 }
+
