11b0a3f82e629a61b0375447ed001c055b78111b
odexi <otakar93@gmail.com>
2017-11-02 21:50:54 +0200

    Marker movement

--- app/src/main/java/com/example/otto/mapboxtest/MainActivity.java
+++ app/src/main/java/com/example/otto/mapboxtest/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.otto.mapboxtest;
 
+import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Parcel;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
@@ -14,6 +16,8 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -45,8 +49,12 @@ public class MainActivity extends AppCompatActivity implements SteerpathMapFragm
     private Marker markerToRemove;
     Button removeMarker;
     private Button disableMarkerAdd;
+    private Button markerMove;
     boolean addableMarkers = true;
     boolean editOn = false;
+    boolean markerDraggable = false;
+    boolean firstClick = false;
+    private Marker markerToMove;
     boolean popupWindowActive = false;
     private String apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZXMiOiJiYXNlOnI7anVoYW5pdGVzdF9zdGF0aWM6cjtqdWhhbml0ZXN0X2R5bmFtaWM6ciIsImp0aSI6ImUyZjY1Mjc0LTY4YTgtNGM0ZS04MGY5LTUzNThkOTBiNTRkNSIsInN1YiI6Imp1aGFuaXRlc3QifQ.gAOJ1h7q43p65H3pXMYsZ2EGYCoCGUTcNhB5aX1s8j4";
 
@@ -57,6 +65,7 @@ public class MainActivity extends AppCompatActivity implements SteerpathMapFragm
         setContentView(R.layout.activity_main);
 
         disableMarkerAdd = (Button)findViewById(R.id.disableMarkerAdd);
+        markerMove = (Button)findViewById(R.id.markerMove);
 
         SteerpathClient.StartConfig config =  new SteerpathClient.StartConfig.Builder()
                 // MANDATORY:
@@ -92,60 +101,69 @@ public class MainActivity extends AppCompatActivity implements SteerpathMapFragm
 
                 map.setCameraPosition(position);
 
+                final BaseMarkerOptions baseMarkerOptions = new BaseMarkerOptions() {
+                    @Override
+                    public BaseMarkerOptions getThis() {
+                        return this;
+                    }
+
+                    @Override
+                    public Marker getMarker() {
+                        return marker;
+                    }
+
+                    @Override
+                    public int describeContents() {
+                        return 0;
+                    }
+
+                    @Override
+                    public void writeToParcel(Parcel dest, int flags) {
+
+                    }
+                };
+
                 mapView.setOnMapClickListener(new MapboxMap.OnMapClickListener() {
                     @Override
                     public void onMapClick(@NonNull LatLng point) {
-                        final BaseMarkerOptions baseMarkerOptions = new BaseMarkerOptions() {
-                            @Override
-                            public BaseMarkerOptions getThis() {
-                                return this;
-                            }
 
-                            @Override
-                            public Marker getMarker() {
-                                return marker;
-                            }
 
-                            @Override
-                            public int describeContents() {
-                                return 0;
-                            }
-
-                            @Override
-                            public void writeToParcel(Parcel dest, int flags) {
-
-                            }
-                        };
-
-                        if (addableMarkers) {
-                            marker = new Marker(baseMarkerOptions);
-                            marker.setPosition(point);
-                            marker.setIcon(IconFactory.getInstance(getApplicationContext()).fromResource(R.drawable.ic_accuracy_marker));
-
-                            SteerpathAnnotationOptions.Builder builder = new SteerpathAnnotationOptions.Builder();
-
-                            SteerpathAnnotationOptions steerpathAnnotationOptions = builder.userId("1").floor(0).withBaseMarkerOptions(baseMarkerOptions).build();
-
-                            map.addAnnotation(steerpathAnnotationOptions);
+                        if(addableMarkers && !markerDraggable){
+                            drawMarker(point, baseMarkerOptions);
                         }
 
+                        else if(firstClick){
+                            drawMarker(point, baseMarkerOptions);
+                            markerToMove.remove();
+                            addableMarkers = false;
+                            firstClick = false;
+                        }
+
+                 mapView.setOnMarkerClickListener(new MapboxMap.OnMarkerClickListener() {
+                     @Override
+                     public boolean onMarkerClick(@NonNull Marker marker) {
+
+                         if(!markerDraggable){
+                             markerToRemove = marker;
+                             showPopupWindow(markerToRemove);
+                         }
+                         else if(markerDraggable){
+                             firstClick = true;
+                             addableMarkers = true;
+                             markerToMove = marker;
+                             markerToMove.setIcon(IconFactory.getInstance(getApplicationContext()).fromResource(R.drawable.ic_local_bar_black_24dp));
+                         }
+
+                         return false;
+                     }
+                 });
+
+
+
+
                     }
                 });
-                mapView.setOnMarkerClickListener(new MapboxMap.OnMarkerClickListener() {
-                    @Override
-                    public boolean onMarkerClick(@NonNull Marker marker) {
-                        markerToRemove = marker;
-
-                        showPopupWindow(markerToRemove);
-
-                        return false;
-                    }
-                });
-
-
-
-
-
+                
                 disableMarkerAdd.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -159,23 +177,78 @@ public class MainActivity extends AppCompatActivity implements SteerpathMapFragm
                     }
                 });
 
+                markerMove.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(!markerDraggable) {
+                            markerDraggable = true;
+                            markerMove.setText("Disable marker moving");
+                        }
+                        else{
+                            markerDraggable = false;
+                            markerMove.setText("Enable marker moving");
+
+                        }
+
+                    }
+                });
+
             }
         });
     }
 
+    private void drawMarker(LatLng point, BaseMarkerOptions baseMarkerOptions){
+        if (addableMarkers) {
+            marker = new Marker(baseMarkerOptions);
+            marker.setPosition(point);
+            marker.setIcon(IconFactory.getInstance(getApplicationContext()).fromResource(R.drawable.ic_accuracy_marker));
+
+            SteerpathAnnotationOptions.Builder builder = new SteerpathAnnotationOptions.Builder();
+
+            SteerpathAnnotationOptions steerpathAnnotationOptions = builder.userId("1").floor(0).withBaseMarkerOptions(baseMarkerOptions).build();
+
+            map.addAnnotation(steerpathAnnotationOptions);
+        }
+    }
+
+    private void dimBehind(PopupWindow popupWindow) {
+        View container;
+        if (popupWindow.getBackground() == null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                container = (View) popupWindow.getContentView().getParent();
+            } else {
+                container = popupWindow.getContentView();
+            }
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                container = (View) popupWindow.getContentView().getParent().getParent();
+            } else {
+                container = (View) popupWindow.getContentView().getParent();
+            }
+        }
+        Context context = popupWindow.getContentView().getContext();
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        WindowManager.LayoutParams p = (WindowManager.LayoutParams) container.getLayoutParams();
+        p.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+        p.dimAmount = 0.5f;
+        wm.updateViewLayout(container, p);
+    }
+
     public void showPopupWindow(Marker marker) {
         this.marker = marker;
-        LinearLayout mainLayout = (LinearLayout) findViewById(R.id.fragment_parent);
 
         LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-        View popupView = inflater.inflate(R.layout.popup_window, null);
+        View popupView = inflater.inflate(R.layout.popup_window, (ViewGroup) findViewById(R.id.popup));
 
-        int width = LinearLayout.LayoutParams.MATCH_PARENT;
-        int height = LinearLayout.LayoutParams.MATCH_PARENT;
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
 
-        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, true);
+        final PopupWindow popupWindow = new PopupWindow(popupView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, true);
 
-        popupWindow.showAtLocation(mainLayout, Gravity.CENTER, 0, 0);
+        popupWindow.showAtLocation(popupView, Gravity.CENTER, 0, 0);
+
+        popupWindow.setHeight(popupWindow.getHeight()-100);
+        dimBehind(popupWindow);
 
         popupView.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -233,5 +306,9 @@ public class MainActivity extends AppCompatActivity implements SteerpathMapFragm
         });
     }
 
+
+
 }
+
+
 
